b314f71ae42b80d1b405878e3dc0a661dffbdd59
Raphaël Sanchez <raphael.sanchez@supinternet.fr>
2016-05-26 19:15:24 +0200

    update UserEvent

--- src/UserBundle/Entity/UserEvent.php
+++ src/UserBundle/Entity/UserEvent.php
@@ -20,6 +20,7 @@ class UserEvent
     protected $id;
 
     /**
+     * @Assert\NotNull()
      * @var boolean
      * @ORM\Column(type="boolean", name="private", nullable=false, options={"default": 0})
      */
@@ -32,4 +33,62 @@ class UserEvent
     * @ORM\Column(type="integer", nullable=false)
     */
     protected $placeAvailable;
+
+
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return UserEvent
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPrivate()
+    {
+        return $this->private;
+    }
+
+    /**
+     * @param boolean $private
+     * @return UserEvent
+     */
+    public function setPrivate($private)
+    {
+        $this->private = $private;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPlaceAvailable()
+    {
+        return $this->placeAvailable;
+    }
+
+    /**
+     * @param int $placeAvailable
+     * @return UserEvent
+     */
+    public function setPlaceAvailable($placeAvailable)
+    {
+        $this->placeAvailable = $placeAvailable;
+        return $this;
+    }
 }
