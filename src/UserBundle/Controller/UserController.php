e7ed54394843fc42e25a7e3f4f86e98a6097541b
Raphaël Sanchez <raphael.sanchez@supinternet.fr>
2016-05-26 17:58:22 +0200

    update userController and add createNewEventAction

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
-    public function indexAction()
+    public function createNewEventAction($id)
     {
-        return $this->render('UserBundle:Default:index.html.twig');
+        $user = $this->getUser();
+        if($user != null)
+        {
+            if($user->getId() == $id)
+            {
+                return $this->render('UserBundle:User:form_new_event.html.twig', array('user' => $user));
+            }
+
+            return $this->render('CoreBundle:Default:404.html.twig');
+        }
     }
 }
