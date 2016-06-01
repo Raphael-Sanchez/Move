23b6da636ebc6f846c63546c32d5dc299f4983d2
Raphaël Sanchez <raphael.sanchez@supinternet.fr>
2016-06-01 14:10:08 +0200

    update event

--- src/EventBundle/Entity/Event.php
+++ src/EventBundle/Entity/Event.php
@@ -28,10 +28,56 @@ class Event
     /**
      * @var integer
      *
-     * @Assert\NotBlank()
      * @ORM\Column(type="integer", nullable=false)
      */
     protected $placeAvailable;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    protected $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    protected $zip;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    protected $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $addressComplement;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $price;
+
+    /**
+     * @ORM\OneToOne(targetEntity="EventBundle\Entity\Activity")
+     * @ORM\JoinColumn(referencedColumnName="id", nullable=true)
+     */
+    protected $activity;
+
+    /**
+     * @ORM\OneToOne(targetEntity="UserBundle\Entity\User", cascade={"persist"})
+     * @ORM\JoinColumn(referencedColumnName="id", nullable=false)
+     */
+    protected $createdBy;
+
 
     public function __construct()
     {
@@ -91,4 +137,131 @@ class Event
         $this->placeAvailable = $placeAvailable;
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     * @return Event
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param string $zip
+     * @return Event
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return Event
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressComplement()
+    {
+        return $this->addressComplement;
+    }
+
+    /**
+     * @param string $addressComplement
+     * @return Event
+     */
+    public function setAddressComplement($addressComplement)
+    {
+        $this->addressComplement = $addressComplement;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     * @return Event
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    /**
+     * @param mixed $activity
+     * @return Event
+     */
+    public function setActivity($activity)
+    {
+        $this->activity = $activity;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param int $createdBy
+     * @return Event
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
 }
